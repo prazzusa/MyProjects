74d100f7d6da690f3060d8259dcf3034b5b5dd72
puspakhanal <puspakhanal@users.noreply.github.com>
2018-02-25 21:23:41 -0500

    created import file to input values for table

---
+++ PizzaOrderSystem/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into topping (description,price) values ('Pepperoni',1.0);
+insert into topping (description,price) values ('Cheese',2.0);
+insert into topping (description,price) values ('Onions',1.0);
+insert into topping (description,price) values ('Pineapple',3.0);
+insert into topping (description,price) values ('Peppers',1.5);
+insert into topping (description,price) values ('Chicken',4.0);
+
